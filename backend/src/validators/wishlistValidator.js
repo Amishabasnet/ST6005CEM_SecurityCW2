01cf59377a0ddc47eb2805a88a0aa7248d4e3979
Amisha Basnet <amishaaaaaa045@gmail.com>
2026-07-15 13:42:21 +0545

    model and validator added

---
+++ backend/src/validators/wishlistValidator.js
@@ -0,0 +1,7 @@
+const { param } = require('express-validator');
+
+const productIdParamValidationRules = [
+  param('productId').isMongoId().withMessage('productId must be a valid product ID'),
+];
+
+module.exports = { productIdParamValidationRules };
